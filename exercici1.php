c54d9c8d495ffe4ecb92ff8649ed7c80b080f11d
MJFrancoM <ma.josefrancom@gmail.com>
2022-03-22 12:05:55 +0100

    Ejercicio 1

---
+++ exercici1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>PHP: Ejercicio 1</title>
+</head>
+<body>
+    <?php 
+    /*Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla. */ 
+
+    $miNumeroInt=2;
+    $miNumeroDou=2.5;
+    $miPalabra="¡Hola!";
+    $logica=true;
+
+    echo $miNumeroInt. " es un número entero <br/>";
+    echo $miNumeroDou. " es un número decimal <br/>";
+    echo $miPalabra. " es una cadena de texto <br/>";
+    echo $logica. " es una variable booleana <br/>";  
+    
+    ?>
+</body>
+</html>
